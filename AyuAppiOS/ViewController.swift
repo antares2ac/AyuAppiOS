29fac81eb61d867da45215c87b7c2e763014836e
Adrian Lucaci <Adrian@macbook-pro-adrian.local>
2018-04-20 21:01:07 +0200

    Initial Commit

---
+++ AyuAppiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AyuAppiOS
+//
+//  Created by Adrian Lucaci on 20/4/18.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AyuAppiOSTests/AyuAppiOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  AyuAppiOSTests.swift
+//  AyuAppiOSTests
+//
+//  Created by Adrian Lucaci on 20/4/18.
+//
+//
+
+import XCTest
+@testable import AyuAppiOS
+
+class AyuAppiOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
